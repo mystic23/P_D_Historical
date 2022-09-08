33086b6b6f45c501315d44db1ee4af2308a5723d
María Isabel Solá Valle <msola@uninorte.edu.co>
2022-09-08 17:52:24 -0500

    sorting agregado pero no sirve

--- Punto_1_1/Java/prueba2.java
+++ Punto_1_1/Java/prueba2.java
@@ -3,7 +3,10 @@ package Punto_1_1.Java;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
 
 
 public class prueba2 {
@@ -73,7 +76,7 @@ public class prueba2 {
                     v_carrera+=(float) (Float.parseFloat(dataGanadores[3])/Float.parseFloat(dataGanadores[4]));
                     total_velocidades+= v_carrera;
                     total_caballo+=v_carrera;
-                    //System.out.println("la velocidad de " + dataCaballo[1] + " es de " + v_carrera +" en la carrera " + dataGanadores[0] );
+                    System.out.println("la velocidad de " + dataCaballo[1] + " es de " + v_carrera +" en la carrera " + dataGanadores[0] );
                                    
                 }
                 Linea_Ganadores = Registro_Ganadores.readLine();
@@ -110,6 +113,112 @@ public class prueba2 {
 
     }
 
+    //el tamaño
+    public static int sizeF(String file_name) {
+        /*
+        Argumentos:
+        String file_name: Nombre del archivo (.txt)
+        
+        Regresa:
+        int result: Numero de lineas en el archivo
+         */
+        int result = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file_name + ".txt"));
+            LineNumberReader count = new LineNumberReader(br);
+            while (count.readLine() != null) {
+            }
+            result = count.getLineNumber();
+            br.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void sortF(String file_name) {
+
+        String data[] = new String[sizeF(file_name)]; // Arreglo almacena contenido de cada linea
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file_name + ".txt")); // Abre archivo
+            String line = null;
+
+            int i = 0;
+            while ((line = br.readLine()) != null) { // Copia contenido 
+                data[i] = line;
+                i++;
+            }
+            if (file_name.equals("Caballos")) {
+                bubble2(data); // Sort data to Sort lines
+            } else {
+                bubble(data);
+            }
+
+            br.close(); // Cierra Archivo
+
+            FileWriter outFile = new FileWriter(file_name + ".txt", false); // Reabre en modo reescritura
+            PrintWriter register = new PrintWriter(outFile);
+
+            for (int j = 0; j < data.length; j++) { // Escribe cada linea del arreglo ordenado en el archivo
+                register.println(data[j]);
+            }
+            register.close(); // Cierra archivo
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static void sw(String[] ls, int i, int j) {
+        /*
+        Argumentos:
+        String[] ls: Arreglo de strings para ordenar
+        int i: i-esimo elemento para intercambiar
+        int j: j-esimo elemento con el que es intercambiado
+        
+         */
+        String ch = ls[i];
+        ls[i] = ls[j];
+        ls[j] = ch;
+    }
+
+    public static void bubble(String [] ls ){
+        int pairs = ls.length;
+        boolean swapped = true;
+        while (swapped) {
+            pairs = pairs - 1;
+            swapped = false;
+            for (int i = 0; i < pairs; i++) {
+                int x = Integer.valueOf(ls[i].split("\t")[0]);
+                // Entero del primer elemento del split del i/j -esimo elemento del arreglo
+                int y = Integer.valueOf(ls[i + 1].split("\t")[0]);
+                if (x > y) {
+                    sw(ls, i, (i + 1));
+                    swapped = true;
+                }
+            }
+        }
+    }
+    public static void bubble2(String[] ls) {
+        
+        int pairs = ls.length;
+        boolean swapped = true;
+        while (swapped) {
+            pairs = pairs - 1;
+            swapped = false;
+            for (int i = 0; i < pairs; i++) {
+                int x = Integer.valueOf(ls[i].split("\t")[0]);
+                int y = Integer.valueOf(ls[i + 1].split("\t")[0]);
+                int x2 = Integer.valueOf(ls[i].split("\t")[2]);
+                int y2 = Integer.valueOf(ls[i + 1].split("\t")[2]);
+                if ((x > y) || ((x == y) && x2 > y2)) {
+                    sw(ls, i, (i + 1));
+                    swapped = true;
+                }
+            }
+        }
+    }
+
 
     public static void main(String [] args) {
         //primera parte 
@@ -120,6 +229,8 @@ public class prueba2 {
         velocidad();
 
         //Ordenar Caballos 
+        sortF("Caballos_desordenados");
+        
 
     }
 
